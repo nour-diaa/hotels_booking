713306bc0a4e333ecf9f1cf35a12587e3be5549e
Ahmad Alsodani <ahmad.alsodani@hyperionstudios.com.au>
2019-04-21 19:39:02 +0800

    add user migration, user factory,seeder, Api Controller

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,10 +17,15 @@ use Faker\Generator as Faker;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+        'phone' => $faker->phoneNumber,
+        'address_id' => $faker->numberBetween(1, 50),
+        'role_id' => $faker->numberBetween(1, 4) ,
+        'user_type' => $faker->randomElement( [ 'owner' , 'employee' , 'customer' ] )
     ];
 });
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,20 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string( "phone" )->nullable();
+
+            $table->unsignedInteger( 'address_id' )->nullable();
+            // TODO: Remove nullable() when ready
+            $table->unsignedInteger( 'role_id' )->nullable();
+
+            // Values ( hotel_owner , hotel_employee , customer, super_admin )
+            $table->string( 'user_type' )->default( 'customer' );
+
             $table->rememberToken();
             $table->timestamps();
         });
